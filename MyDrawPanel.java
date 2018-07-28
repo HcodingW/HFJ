6db79bfab4e067626b5a92a32d3cf0247f8be704
HcodingW <2294103317@qq.com>
2018-07-28 10:48:47 +0800

    Add files via upload
    //with TwoButtons

---
+++ MyDrawPanel.java
@@ -0,0 +1,16 @@
+package try1;
+
+import java.awt.*;
+import javax.swing.*;
+
+//with TwoButtons
+
+class MyDrawPanel extends JPanel{
+	public void paintComponent(Graphics g) {
+		Graphics2D g2d = (Graphics2D) g;
+		GradientPaint gradient = new GradientPaint(70, 70, Color.blue, 150, 150, Color.orange);
+		g2d.setPaint(gradient);
+		g2d.fillOval(80, 80, 100, 100);
+		System.out.println("haha");
+	}	
+}
